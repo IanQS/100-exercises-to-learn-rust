dce706cd81c47956be9205817f40c8a318353dff
IQ <ian@ianq.ai>
2026-03-18 10:24:10 -0700

    finished 2 and half of 3

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,13 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut accum = 1;
+    let mut tracker = 1;
+    while tracker <= n {
+        accum *= tracker;
+        tracker += 1
+    }
+    accum
 }
 
 #[cfg(test)]
