6044e3e6a1dd64d14063342fe03712a0131c3020
IQ <ian@ianq.ai>
2026-03-19 10:54:49 -0700

    save

--- exercises/03_ticket_v1/07_setters/src/lib.rs
+++ exercises/03_ticket_v1/07_setters/src/lib.rs
@@ -11,26 +11,38 @@ pub struct Ticket {
 
 impl Ticket {
     pub fn new(title: String, description: String, status: String) -> Ticket {
+        Ticket::title_validator(&title);
+        Ticket::description_validator(&description);
+        Ticket::status_validator(&status);
+
+        Ticket {
+            title,
+            description,
+            status,
+        }
+    }
+
+    pub fn title_validator(title: &String) {
         if title.is_empty() {
             panic!("Title cannot be empty");
         }
         if title.len() > 50 {
             panic!("Title cannot be longer than 50 bytes");
         }
-        if description.is_empty() {
+    }
+
+    pub fn description_validator(desc: &String) {
+        if desc.is_empty() {
             panic!("Description cannot be empty");
         }
-        if description.len() > 500 {
+        if desc.len() > 500 {
             panic!("Description cannot be longer than 500 bytes");
         }
+    }
+
+    pub fn status_validator(status: &String) {
         if status != "To-Do" && status != "In Progress" && status != "Done" {
             panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
-        }
-
-        Ticket {
-            title,
-            description,
-            status,
         }
     }
 
@@ -44,6 +56,19 @@ impl Ticket {
 
     pub fn status(&self) -> &String {
         &self.status
+    }
+
+    pub fn set_title(&mut self, new_title: String) {
+        Ticket::title_validator(&new_title);
+        self.title = new_title
+    }
+    pub fn set_description(&mut self, new_description: String) {
+        Ticket::description_validator(&new_description);
+        self.description = new_description
+    }
+    pub fn set_status(&mut self, new_status: String) {
+        Ticket::status_validator(&new_status);
+        self.status = new_status
     }
 }
 
